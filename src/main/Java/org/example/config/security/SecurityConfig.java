eb5b318defda8ecfa9a9cb78ec8bd95b522ab11f
anjik777 <anzhelali87@gmail.com>
2021-10-14 16:19:08 +0900

    changes in SC

--- src/main/Java/org/example/config/security/SecurityConfig.java
+++ src/main/Java/org/example/config/security/SecurityConfig.java
@@ -1,41 +1,30 @@
 package org.example.config.security;
 
 import org.example.config.handler.SuccessUserHandler;
-import org.example.service.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 
 @Configuration
+
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private UserServiceImpl userServiceImpl;
+    private final UserDetailsService userDetailsService;// сервис, с помощью которого тащим пользователя
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserDetailsService userDetailsService() {
-        return userServiceImpl;
-    }
+    public SecurityConfig(UserDetailsService userDetailsService, PasswordEncoder passwordEncoder ) {
 
-    private UserDetailsService userDetailsService;// сервис, с помощью которого тащим пользователя
-    private SuccessUserHandler successUserHandler;// класс, в котором описана логика перенаправления пользователей по ролям
-    private PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-
-    public SecurityConfig(@Lazy @Qualifier("userServiceImpl") UserDetailsService userDetailsService,
-                          SuccessUserHandler successUserHandler) {
+        this.passwordEncoder = passwordEncoder;
         this.userDetailsService = userDetailsService;
-        this.successUserHandler = successUserHandler;
     }
 
     @Autowired
@@ -45,25 +34,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests();
 
-        http.csrf().disable().authorizeRequests(); //- попробуйте выяснить сами, что это даёт
         http.formLogin()
                 .successHandler(new SuccessUserHandler())
                 .permitAll();
+
         http.logout()
                 .permitAll()
                 .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
                 .logoutSuccessUrl("/login")
                 .and().csrf().disable();
+
         http
                 .authorizeRequests()
-                .antMatchers("/admin/**").access("hasRole('ADMIN')")
-                .antMatchers("/user/**").access("hasAnyRole('USER', 'ADMIN')")
+                .antMatchers("/admin/**").access("hasAnyRole('ADMIN', 'USER')")
                 .anyRequest().authenticated();
-    }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 }
--- src/main/Java/org/example/service/UserServiceImpl.java
+++ src/main/Java/org/example/service/UserServiceImpl.java
@@ -6,6 +6,7 @@ import org.example.dao.UserDaoImpl;
 import org.example.model.Role;
 import org.example.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -26,15 +27,16 @@ import java.util.Set;
 public class UserServiceImpl implements UserService, UserDetailsService {
 
     private UserDao userDao;
-    private RoleDao roleDao;
     private PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserServiceImpl(UserDao userDao, RoleDao roleDao, PasswordEncoder passwordEncoder) {
-
-        this.passwordEncoder = passwordEncoder;
+    public UserServiceImpl(UserDao userDao) {
         this.userDao = userDao;
-        this.roleDao = roleDao;
+    }
+
+    @Autowired
+    public void setPasswordEncoder(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
     }
 
     public UserServiceImpl() {
@@ -43,16 +45,14 @@ public class UserServiceImpl implements UserService, UserDetailsService {
     @Transactional
     @Override
     public User justSaveUser(User user) {  //метод для addInitData(), который заполняет базу данных при
-        return userDao.justSaveUser(user); //для метода Init - при старте приложения
+        return userDao.justSaveUser(user);
     }
 
     @Override
     public User registerUser(User user, Set<Role> rolesFromCheckbox) { //метод для сохранения новых юзеров
         User userFromDb = userDao.getUserByName(user.getUsername());
-        //Role role = roleDao.findRoleByRoleName("ROLE_USER");
 
         if (userFromDb == null) {
-            // user.setRoles(new HashSet<>(Arrays.asList(role)));
             user.setRoles(rolesFromCheckbox);
             user.setPassword(passwordEncoder.encode(user.getPassword()));
             userDao.justSaveUser(user);
@@ -76,7 +76,6 @@ public class UserServiceImpl implements UserService, UserDetailsService {
     @Override
     public User updateUser(User user, Set<Role> roles) { //метод для редактирования user-данных
 
-        //user.setRoles(new HashSet<>(Arrays.asList(roleDao.findByRole("ROLE_USER"))));
         user.setRoles(roles); //роли должны быть после merge(updateRole)
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userDao.updateUser(user);
@@ -105,7 +104,11 @@ public class UserServiceImpl implements UserService, UserDetailsService {
         for (Role role : user.getRoles()) {
             grantedAuthorities.add(new SimpleGrantedAuthority(role.getRole()));
         }
-        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
-                grantedAuthorities);
+        return user;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
